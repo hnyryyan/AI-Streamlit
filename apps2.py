5e3f2148ce157abf995e2c411f79ffbfa9b94f5e
ryyan <barsay090@gmail.com>
2026-05-05 10:25:43 +0700

    Update apps2.py

--- apps2.py
+++ apps2.py
@@ -23,6 +23,10 @@ st.markdown("""
         padding: 15px;
         border-radius: 10px;
         box-shadow: 0 2px 4px rgba(0,0,0,0);
+        color: #000000;
+    }
+    .stMetric * {
+        color: #000000 !important;
     }
     </style>
     """, unsafe_allow_html=True)
